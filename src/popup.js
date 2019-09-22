29b15610584cc30b3ffde5a9a3aa4d0375500d90
Ole Lübke <de.toolcreator@gmail.com>
2019-09-22 18:40:24 +0200

    Change license to MIT

--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,17 @@
 /*
- * Copyright 2018 Ole Lübke
+ * Copyright (c) 2019 Ole Lübke
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * Permission to use, copy, modify, and/or distribute this software
- * for any purpose with or without fee is hereby granted,
- * provided that the above copyright notice and this permission notice appear
- * in all copies.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
- * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
- * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
- * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
- * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 var copyChk = document.getElementById("copyChk");
@@ -22,9 +21,9 @@ var cardListTxt = document.getElementById("cardListTxt");
 
 chrome.storage.sync.get(
   ["autocopy"],
-  function(result) {
+  function (result) {
     "use strict";
-    if(result) {
+    if (result) {
       copyChk.checked = result.autocopy;
     }
   }
@@ -36,45 +35,45 @@ function copyCardListToClipboard() {
   document.execCommand("copy");
 }
 
-getCardsBtn.onclick = function() {
+getCardsBtn.onclick = function () {
   "use strict";
   chrome.tabs.query(
     {
       "currentWindow": true,
       "url": [
         "https://scryfall.com/card/*"
-        ,"https://edhrec.com/cards/*"
-        ,"https://www.cardmarket.com/*/Magic/Products/Singles/*"
-        ,"https://tappedout.net/mtg-card/*"
-        ,"http://gatherer.wizards.com/Pages/Card/*"
-        ,"https://www.mtg-forum.de/db/magiccard.php*"
+        , "https://edhrec.com/cards/*"
+        , "https://www.cardmarket.com/*/Magic/Products/Singles/*"
+        , "https://tappedout.net/mtg-card/*"
+        , "http://gatherer.wizards.com/Pages/Card/*"
+        , "https://www.mtg-forum.de/db/magiccard.php*"
       ]
     },
-    function(tabs) {
+    function (tabs) {
       var list = "";
-      tabs.forEach(function(tab) {
-        if(tab.url.includes("scryfall")) {
-          list += tab.title.substring(0,tab.title.indexOf("\u00b7"))
-                  .trim() + "\n";
-        } else if(tab.url.includes("edhrec")) {
+      tabs.forEach(function (tab) {
+        if (tab.url.includes("scryfall")) {
+          list += tab.title.substring(0, tab.title.indexOf("\u00b7"))
+            .trim() + "\n";
+        } else if (tab.url.includes("edhrec")) {
           list += tab.title.substring(
-                    tab.title.indexOf("-") + 1, tab.title.indexOf("("))
-                  .trim() + "\n";
-        } else if(tab.url.includes("cardmarket")) {
+            tab.title.indexOf("-") + 1, tab.title.indexOf("("))
+            .trim() + "\n";
+        } else if (tab.url.includes("cardmarket")) {
           list += tab.title.substring(0, tab.title.indexOf("|"))
-                  .trim() + "\n";
-        } else if(tab.url.includes("tappedout") || tab.url.includes("gatherer")) {
+            .trim() + "\n";
+        } else if (tab.url.includes("tappedout") || tab.url.includes("gatherer")) {
           list += tab.title.substring(0, tab.title.indexOf("("))
-                  .trim() + "\n";
-        } else if(tab.url.includes("mtg-forum")) {
+            .trim() + "\n";
+        } else if (tab.url.includes("mtg-forum")) {
           list += tab.title.substring(0, tab.title.indexOf("-"))
-                  .trim() + "\n";
+            .trim() + "\n";
         }
       });
       cardListTxt.innerHTML = list;
-      if(list.trim() !== "") {
+      if (list.trim() !== "") {
         alert("Found " + tabs.length + " tabs with cards");
-        if(copyChk.checked) {
+        if (copyChk.checked) {
           copyCardListToClipboard();
         }
       } else {
@@ -86,7 +85,7 @@ getCardsBtn.onclick = function() {
 
 copyBtn.onclick = copyCardListToClipboard;
 
-copyChk.onchange = function() {
+copyChk.onchange = function () {
   "use strict";
   chrome.storage.sync.set(
     {
